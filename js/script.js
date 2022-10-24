843ff5973e73f02d422da28b201bc8eff2c44b65
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-10-24 17:02:48 +0200

    final changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,59 @@
 console.log('Calcolo prezzo del biglietto')
 
-const name = document.querySelector('nome')
-const distance=document.querySelector('km')
-const age= document.querySelector('eta')
-const conferme=document.querySelector('confirm')
-const deconste=document.querySelector('delete')
+let inputName = document.getElementById ('name')
+let inputKm = document.getElementById('km');
+let inputAge= document.getElementById('age')
+
+let price= inputKm * 0.21
+let underage = price* 0.2
+let overage = price * 0.4
+
+const btnGenerate=document.querySelector('confirm')
+const btnReset=document.querySelector('delete')
+const eleTicket = document.querySelector('.ticket')
+
+const outputName=document.getElementById('name-pass')
+const outputPrice=document.getElementById('price-pass')
+
+const pricePerKilometer = 0.21;
 
 conferme.addEventListener('click', function() {
-    const distance = parseFloat(distance.value);
+    const distance = parseFloat(inputKm.value);
+	const age = inputAge.value;
+
+    if(inputName.value != '' && inputAge.value != '' && inputAge.value!='seleziona' ){
+        const baseprice= pricePerKilometer * distance;
+        let discountRate;
+
+        switch (age){
+            case 'minorenne':
+                discountRate=20;
+                break;
+            case 'maggiorenne':
+                discountRate=0;
+                break;
+            case 'over':
+                discountRate=40;
+                break;
+        }
+        let finalPrice
+        finalPrice= price - (discountRate*price) / 100;
+
+        console.log('il prezzo del biglietto è', finalPrice,'€');
+
+        outputName.innerHTML= inputName.value;
+        outputPrice.innerHTML=finalPrice;
+        eleTicket.classList.remove('hide')
+
+    } else{
+        console.log('Inserire dei valori');
+    }
 
 });
 console.log('nome', name, 'km', distance, 'età', age)
+
+discard.addEventListener('click', function () {
+	outputName.innerHTML = '';
+	outputPrice.innerHTML = '';
+	eleTicket.classList.add('hidden');
+});
